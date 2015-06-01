65d8cb7143b823debfa934e17367beca636379dc
Tasos K (@tenovo) <anastasios.koutoumanos@gmail.com>
2015-06-01 19:13:57 +0300

    Saleh's first version of the toolkit

---
+++ js/slider.js
@@ -0,0 +1,407 @@
+		
+				$(document).ready(function(){
+   				$("#flip").click(function(){
+   					if($("#panela").is(":visible")){
+   						$("#panela").slideToggle("slow");
+   					}
+   					else if($("#panelb").is(":visible")){
+   						$("#panelb").slideToggle("slow");
+   					}
+   					else if($("#panelc").is(":visible")){
+   						$("#panelc").slideToggle("slow");
+   					}
+   					else if($("#paneld").is(":visible")){
+   						$("#paneld").slideToggle("slow");
+   					}
+					else if($("#panele").is(":visible")){
+   						$("#panele").slideToggle("slow");
+   					}
+
+       			$("#panel").slideToggle("slow");
+
+   					});
+				});
+
+				
+				$(document).ready(function(){
+   				$("#flipa").click(function(){
+   					if($("#panel").is(":visible")){
+   						$("#panel").slideToggle("slow");
+   					}
+   					else if($("#panelb").is(":visible")){
+   						$("#panelb").slideToggle("slow");
+   					}
+   					else if($("#panelc").is(":visible")){
+   						$("#panelc").slideToggle("slow");
+   					}
+   					else if($("#paneld").is(":visible")){
+   						$("#paneld").slideToggle("slow");
+   					}
+					else if($("#panele").is(":visible")){
+   						$("#panele").slideToggle("slow");
+   					}
+
+   				
+       			$("#panela").slideToggle("slow");
+
+   					});
+				});
+
+				
+				$(document).ready(function(){
+   				$("#flipb").click(function(){
+   				if($("#panel").is(":visible")){
+   						$("#panel").slideToggle("slow");
+   					}
+   					else if($("#panela").is(":visible")){
+   						$("#panela").slideToggle("slow");
+   					}
+   					else if($("#panelc").is(":visible")){
+   						$("#panelc").slideToggle("slow");
+   					}
+   					else if($("#paneld").is(":visible")){
+   						$("#paneld").slideToggle("slow");
+   					}
+					else if($("#panele").is(":visible")){
+   						$("#panele").slideToggle("slow");
+   					}
+       			$("#panelb").slideToggle("slow");
+   					});
+				});
+
+		
+				$(document).ready(function(){
+   				$("#flipc").click(function(){
+   				if($("#panel").is(":visible")){
+   						$("#panel").slideToggle("slow");
+   					}
+   					else if($("#panelb").is(":visible")){
+   						$("#panelb").slideToggle("slow");
+   					}
+   					else if($("#panela").is(":visible")){
+   						$("#panela").slideToggle("slow");
+   					}
+   					else if($("#paneld").is(":visible")){
+   						$("#paneld").slideToggle("slow");
+   					}
+					else if($("#panele").is(":visible")){
+   						$("#panele").slideToggle("slow");
+   					}
+       			$("#panelc").slideToggle("slow");
+   					});
+				});
+
+		
+				$(document).ready(function(){
+   				$("#flipd").click(function(){
+					if($("#panel").is(":visible")){
+   						$("#panel").slideToggle("slow");
+   					}
+   					else if($("#panelb").is(":visible")){
+   						$("#panelb").slideToggle("slow");
+   					}
+   					else if($("#panelc").is(":visible")){
+   						$("#panelc").slideToggle("slow");
+   					}
+   					else if($("#panela").is(":visible")){
+   						$("#panela").slideToggle("slow");
+   					}
+					else if($("#panele").is(":visible")){
+   						$("#panele").slideToggle("slow");
+   					}       			
+   					$("#paneld").slideToggle("slow");
+   					});
+				});
+
+		
+				$(document).ready(function(){
+   				$("#flipbox").click(function(){
+   				$("#paneld").slideToggle("slow");
+       			$("#panelbox").slideToggle("slow");
+   					});
+				});
+
+				$(document).ready(function(){
+   				$("#flipe").click(function(){
+   					if($("#panela").is(":visible")){
+   						$("#panela").slideToggle("slow");
+   					}
+   					if($("#panelb").is(":visible")){
+   						$("#panelb").slideToggle("slow");
+   					}
+   					if($("#panelc").is(":visible")){
+   						$("#panelc").slideToggle("slow");
+   					}
+   					if($("#paneld").is(":visible")){
+   						$("#paneld").slideToggle("slow");
+   					}
+
+					if($("#panel").is(":visible")){
+   						$("#panel").slideToggle("slow");
+   					}     
+		$("#panele").slideToggle("slow");
+   					});
+				});
+
+/**************************************************************************************************************************************/
+
+				$(document).ready(function(){
+   				$("#flipaa").click(function(){
+
+       			$("#panelaa").slideToggle("slow");
+
+   					});
+				});
+
+/***********************************************/
+
+				$(document).ready(function(){
+   				$("#flipab").click(function(){
+   					if($("#panelac").is(":visible")){
+   						$("#panelac").slideToggle("slow");
+   					}
+   					else if($("#panelad").is(":visible")){
+   						$("#panelad").slideToggle("slow");
+   					}
+   					else if($("#panelae").is(":visible")){
+   						$("#panelae").slideToggle("slow");
+   					}
+
+       			$("#panelab").slideToggle("slow");
+
+   					});
+				});
+
+				
+				$(document).ready(function(){
+   				$("#flipac").click(function(){
+   					if($("#panelab").is(":visible")){
+   						$("#panelab").slideToggle("slow");
+   					}
+   					else if($("#panelae").is(":visible")){
+   						$("#panelae").slideToggle("slow");
+   					}
+   					else if($("#panelad").is(":visible")){
+   						$("#panelad").slideToggle("slow");
+   					}
+   				
+       			$("#panelac").slideToggle("slow");
+
+   					});
+				});
+
+				
+				$(document).ready(function(){
+   				$("#flipad").click(function(){
+   				if($("#panelab").is(":visible")){
+   						$("#panelab").slideToggle("slow");
+   					}
+   					else if($("#panelac").is(":visible")){
+   						$("#panelac").slideToggle("slow");
+   					}
+   					else if($("#panelae").is(":visible")){
+   						$("#panelae").slideToggle("slow");
+   					}
+
+       			$("#panelad").slideToggle("slow");
+   					});
+				});
+
+		
+				$(document).ready(function(){
+   				$("#flipae").click(function(){
+   				if($("#panelab").is(":visible")){
+   						$("#panelab").slideToggle("slow");
+   					}
+   					else if($("#panelac").is(":visible")){
+   						$("#panelac").slideToggle("slow");
+   					}
+   					else if($("#panelad").is(":visible")){
+   						$("#panelad").slideToggle("slow");
+   					}
+
+       			$("#panelae").slideToggle("slow");
+   					});
+				});
+
+/************************************************************************************************************************************/
+
+		
+				$(document).ready(function(){
+   				$("#flipaq").click(function(){
+   					if($("#panelaw").is(":visible")){
+   						$("#panelaw").slideToggle("slow");
+   					}
+   					else if($("#panelar").is(":visible")){
+   						$("#panelar").slideToggle("slow");
+   					}
+   					else if($("#panelat").is(":visible")){
+   						$("#panelat").slideToggle("slow");
+   					}
+   					else if($("#panelay").is(":visible")){
+   						$("#panelay").slideToggle("slow");
+   					}
+
+       			$("#panelaq").slideToggle("slow");
+
+   					});
+				});
+
+/*****************/
+
+		
+				$(document).ready(function(){
+   				$("#flipaw").click(function(){
+   					if($("#panelaq").is(":visible")){
+   						$("#panelaq").slideToggle("slow");
+   					}
+   					else if($("#panelar").is(":visible")){
+   						$("#panelar").slideToggle("slow");
+   					}
+   					else if($("#panelat").is(":visible")){
+   						$("#panelat").slideToggle("slow");
+   					}
+   					else if($("#panelay").is(":visible")){
+   						$("#panelay").slideToggle("slow");
+   					}
+
+       			$("#panelaw").slideToggle("slow");
+
+   					});
+				});
+
+/******************/
+
+		
+				$(document).ready(function(){
+   				$("#flipar").click(function(){
+   					if($("#panelaq").is(":visible")){
+   						$("#panelaq").slideToggle("slow");
+   					}
+   					else if($("#panelaw").is(":visible")){
+   						$("#panelaw").slideToggle("slow");
+   					}
+   					else if($("#panelat").is(":visible")){
+   						$("#panelat").slideToggle("slow");
+   					}
+   					else if($("#panelay").is(":visible")){
+   						$("#panelay").slideToggle("slow");
+   					}
+
+       			$("#panelar").slideToggle("slow");
+
+   					});
+				});
+
+/*********************/
+
+		
+				$(document).ready(function(){
+   				$("#flipat").click(function(){
+   					if($("#panelaq").is(":visible")){
+   						$("#panelaq").slideToggle("slow");
+   					}
+   					else if($("#panelaw").is(":visible")){
+   						$("#panelaw").slideToggle("slow");
+   					}
+   					else if($("#panelar").is(":visible")){
+   						$("#panelar").slideToggle("slow");
+   					}
+   					else if($("#panelay").is(":visible")){
+   						$("#panelay").slideToggle("slow");
+   					}
+
+       			$("#panelat").slideToggle("slow");
+
+   					});
+				});
+
+/***********************/
+
+
+		
+				$(document).ready(function(){
+   				$("#flipay").click(function(){
+   					if($("#panelaq").is(":visible")){
+   						$("#panelaq").slideToggle("slow");
+   					}
+   					else if($("#panelaw").is(":visible")){
+   						$("#panelaw").slideToggle("slow");
+   					}
+   					else if($("#panelar").is(":visible")){
+   						$("#panelar").slideToggle("slow");
+   					}
+   					else if($("#panelat").is(":visible")){
+   						$("#panelat").slideToggle("slow");
+   					}
+
+       			$("#panelay").slideToggle("slow");
+
+   					});
+				});
+
+/*************************/
+
+
+
+
+
+				$(document).ready(function(){
+   				$("#flipaf").click(function(){
+   					if($("#panelag").is(":visible")){
+   						$("#panelag").slideToggle("slow");
+   					}
+   					else if($("#panelah").is(":visible")){
+   						$("#panelah").slideToggle("slow");
+   					}
+
+       			$("#panelaf").slideToggle("slow");
+
+   					});
+				});
+
+/*********************/
+
+		
+				$(document).ready(function(){
+   				$("#flipag").click(function(){
+   					if($("#panelaf").is(":visible")){
+   						$("#panelaf").slideToggle("slow");
+   					}
+   					else if($("#panelah").is(":visible")){
+   						$("#panelah").slideToggle("slow");
+   					}
+
+       			$("#panelag").slideToggle("slow");
+
+   					});
+				});
+
+/***********************/
+
+
+		
+				$(document).ready(function(){
+   				$("#flipah").click(function(){
+   					if($("#panelaf").is(":visible")){
+   						$("#panelaf").slideToggle("slow");
+   					}
+   					else if($("#panelag").is(":visible")){
+   						$("#panelag").slideToggle("slow");
+   					}
+
+
+       			$("#panelah").slideToggle("slow");
+
+   					});
+				});
+
+/*************************/
+
+
+
+
+
+
+
+
