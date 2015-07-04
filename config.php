ba64218a36f01e0c6a4203c23f4f2797b4c43082
saleh <ssalem@eummena.org>
2015-07-05 00:07:20 +0300

    1

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysql_host = "localhost";
-$mysql_database = "dbtest"; 
-$mysql_user = "root";
-$mysql_password = "";
-
-mysql_connect($mysql_host,$mysql_user,$mysql_password);
-mysql_select_db($mysql_database); 
-?>
