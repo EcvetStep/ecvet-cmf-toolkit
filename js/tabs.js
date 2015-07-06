58ddb479543fabc3f37fadb468aa4ae2882de147
saleh <ssalem@eummena.org>
2015-07-07 02:28:45 +0300

    Image zoom for levels updated

--- js/tabs.js
+++ js/tabs.js
@@ -26,3 +26,38 @@
 
   }
  }
+
+$('#zoom_01').elevateZoom({
+    zoomType: "inner",
+cursor: "crosshair",
+zoomWindowFadeIn: 500,
+zoomWindowFadeOut: 750
+   }); 
+
+    $('#zoom_02').elevateZoom({
+    zoomType: "inner",
+cursor: "crosshair",
+zoomWindowFadeIn: 500,
+zoomWindowFadeOut: 750
+   }); 
+
+    $('#zoom_03').elevateZoom({
+    zoomType: "inner",
+cursor: "crosshair",
+zoomWindowFadeIn: 500,
+zoomWindowFadeOut: 750
+   }); 
+
+    $('#zoom_04').elevateZoom({
+    zoomType: "inner",
+cursor: "crosshair",
+zoomWindowFadeIn: 500,
+zoomWindowFadeOut: 750
+   }); 
+
+    $('#zoom_05').elevateZoom({
+    zoomType: "inner",
+cursor: "crosshair",
+zoomWindowFadeIn: 500,
+zoomWindowFadeOut: 750
+   }); 
