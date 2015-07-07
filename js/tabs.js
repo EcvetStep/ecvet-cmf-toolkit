515665dda4430ecb0a5294d7f21e3fc4846c52ba
saleh <ssalem@eummena.org>
2015-07-07 14:10:35 +0300

    Image zoom for each level

--- js/tabs.js
+++ js/tabs.js
@@ -27,37 +27,3 @@
   }
  }
 
-$('#zoom_01').elevateZoom({
-    zoomType: "inner",
-cursor: "crosshair",
-zoomWindowFadeIn: 500,
-zoomWindowFadeOut: 750
-   }); 
-
-    $('#zoom_02').elevateZoom({
-    zoomType: "inner",
-cursor: "crosshair",
-zoomWindowFadeIn: 500,
-zoomWindowFadeOut: 750
-   }); 
-
-    $('#zoom_03').elevateZoom({
-    zoomType: "inner",
-cursor: "crosshair",
-zoomWindowFadeIn: 500,
-zoomWindowFadeOut: 750
-   }); 
-
-    $('#zoom_04').elevateZoom({
-    zoomType: "inner",
-cursor: "crosshair",
-zoomWindowFadeIn: 500,
-zoomWindowFadeOut: 750
-   }); 
-
-    $('#zoom_05').elevateZoom({
-    zoomType: "inner",
-cursor: "crosshair",
-zoomWindowFadeIn: 500,
-zoomWindowFadeOut: 750
-   }); 
