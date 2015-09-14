079a029b0dffd592f250740b7378094835647bd1
Tasos K (@tenovo) <anastasios.koutoumanos@gmail.com>
2015-09-14 14:51:21 +0300

    Fixed bug with remaining content of previous tabs

--- js/tabs.js
+++ js/tabs.js
@@ -10,20 +10,19 @@ tabs = {
 
             var th = $(this),
                 tContent = $('.tab-content', th),
-                navA = $('.nav a', th)
+                navA = $('.nav a', th);
 
-            tContent.not(tContent.eq(0)).hide()
+            tContent.not(tContent.eq(0)).hide();
 
             navA.click(function() {
                 var th = $(this),
-                    tmp = th.attr('href')
-                tContent.not($(tmp.slice(tmp.indexOf('#'))).fadeIn(1000)).hide()
+                    tmp = th.attr('href');
+                tContent.not($(tmp.slice(tmp.indexOf('#'))).fadeIn(1000)).hide();
                 $(th).parent().addClass('selected').siblings().removeClass('selected').find('span').stop().animate({
                     opacity: '0'
                 }, 600);
                 return false;
             });
         });
-
     }
 }
